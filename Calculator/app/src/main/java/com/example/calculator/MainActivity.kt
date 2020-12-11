63d6e8553a7d1a3226d8669ca3e337b137495031
Vera Debora Vitamas <veradebora@outlook.co.id>
2020-12-12 01:48:04 +0700

    Fixing landscape layout, and also adding Neg button for negative number

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 private const val STATE_PENDING_OPERATION = "PendingOperation"
@@ -13,9 +11,6 @@ private const val STATE_OPERAND1 = "Operand1"
 private const val STATE_OPERAND1_STORED = "Operand1_Stored"
 
 class MainActivity : AppCompatActivity() {
-//    private lateinit var result: EditText
-//    private lateinit var newNumber: EditText
-//    private val displayOperation by lazy(LazyThreadSafetyMode.NONE) { findViewById<TextView>(R.id.operation) }
 
     //Variable to hold the operands and type of calculation
     private var operand1: Double? = null
@@ -24,30 +19,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        result = findViewById(R.id.result)
-//        newNumber = findViewById(R.id.newNumber)
-
-        // Data input buttons
-
-//        val button0: Button = findViewById(R.id.button0)
-//        val button1: Button = findViewById(R.id.button1)
-//        val button2: Button = findViewById(R.id.button2)
-//        val button3: Button = findViewById(R.id.button3)
-//        val button4: Button = findViewById(R.id.button4)
-//        val button5: Button = findViewById(R.id.button5)
-//        val button6: Button = findViewById(R.id.button6)
-//        val button7: Button = findViewById(R.id.button7)
-//        val button8: Button = findViewById(R.id.button8)
-//        val button9: Button = findViewById(R.id.button9)
-//        val buttonDot: Button = findViewById(R.id.buttonDot)
-
-        // Operation buttons
-//        val buttonEquals = findViewById<Button>(R.id.buttonEquals)
-//        val buttonDivide = findViewById<Button>(R.id.buttonDivide)
-//        val buttonMultiply = findViewById<Button>(R.id.buttonMultiply)
-//        val buttonMinus = findViewById<Button>(R.id.buttonMinus)
-//        val buttonPlus = findViewById<Button>(R.id.buttonPlus)
 
         val listener= View.OnClickListener { v ->
             val b = v as Button
@@ -83,7 +54,24 @@ class MainActivity : AppCompatActivity() {
         buttonMultiply.setOnClickListener(opListener)
         buttonPlus.setOnClickListener(opListener)
         buttonMinus.setOnClickListener(opListener)
+
+        buttonNeg.setOnClickListener({ view ->
+            val value = newNumber.text.toString()
+            if (value.isEmpty()) {
+                newNumber.setText("-")
+            } else {
+                try {
+                    var doubleValue = value.toDouble()
+                    doubleValue *= -1
+                    newNumber.setText(doubleValue.toString())
+                } catch (e: NumberFormatException) {
+                    // newNumber wa "-" or ".", so clear it
+                    newNumber.setText("")
+                }
+            }
+        })
      }
+
 
     private fun performOperation(value: Double, operation: String) {
         if (operand1 == null) {
