3f34791cebe839eeda8a7bcbb44c2d9d6bac7f98
Vera Debora Vitamas <veradebora@outlook.co.id>
2020-12-17 15:56:35 +0700

    Parsing the data

--- Top 10 Downloader/app/src/main/java/com/example/top10downloader/MainActivity.kt
+++ Top 10 Downloader/app/src/main/java/com/example/top10downloader/MainActivity.kt
@@ -15,6 +15,23 @@ import java.net.MalformedURLException
 import java.net.URL
 import java.security.cert.CertPath
 
+class FeedEntry {
+    var name: String = ""
+    var artist: String = ""
+    var releaseDate: String = ""
+    var summary: String = ""
+    var imageURL: String = ""
+
+    override fun toString(): String {
+        return """
+            name = $name
+            artist = $artist
+            releaseDate = $releaseDate
+            imageURL = $imageURL
+         """.trimIndent()
+    }
+}
+
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
 
---
+++ Top 10 Downloader/app/src/main/java/com/example/top10downloader/ParseApplications.kt
@@ -0,0 +1,36 @@
+package com.example.top10downloader
+
+import android.util.Log
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
+
+class ParseApplications {
+    private val TAG = "ParseApplications"
+    val applications = ArrayList<FeedEntry>()
+
+
+    fun parse(xmlData: String): Boolean {
+        Log.d(TAG, "parse called with $xmlData")
+        var status = true
+        var inEntry = false
+        var textValue = ""
+
+        try {
+            val factory = XmlPullParserFactory.newInstance()
+            factory.isNamespaceAware = true
+            val xpp = factory.newPullParser()
+            xpp.setInput(xmlData.reader())
+            var eventType = xpp.eventType
+            var currentRecord = FeedEntry()
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+
+            }
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            status = false
+        }
+
+        return status
+    }
+}
