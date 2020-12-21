c20078b4f2716d81610e578d7be75882af5fd224
Vera Debora Vitamas <veradebora@outlook.co.id>
2020-12-21 15:26:02 +0700

    showing all list of data the been parsed

--- Top 10 Downloader/app/src/main/java/com/example/top10downloader/ParseApplications.kt
+++ Top 10 Downloader/app/src/main/java/com/example/top10downloader/ParseApplications.kt
@@ -23,7 +23,7 @@ class ParseApplications {
 
             xpp.setInput(xmlData.reader())
             // 24 we have a valid PullParser object, so we can tell it what to parse by giving it a StringReader, that's using the xmlData String.
-            //Now a StringReader, by the way, is a class that treats a String like a stream. So think of it as an efficient way of processing
+            //Now a StringReader, by the way, is a class that treats a String like a stream. So think of it as an efficient waxy of processing
             //strings, and the way that the XMLPull Parser does things. Now you probably saw it pop up that it wanted a Reader when I
             //typed the opening bracket after set  Input. So the XMLPullParser needs a StringReader to read from,
             //and a String Reader needs a String to read - which is what we've got on line 25 there. Kotlin makes that a lot easier.
@@ -31,7 +31,46 @@ class ParseApplications {
             var eventType = xpp.eventType
             var currentRecord = FeedEntry()
             while (eventType != XmlPullParser.END_DOCUMENT) {
+                // 33 check the event, and make sure that we haven't reached the end of the document
+                val tagName = xpp.name?.toLowerCase() //TODO: we should use the safe-calll operator?
+                when (eventType) {
 
+                    XmlPullParser.START_TAG -> {
+                        Log.d(TAG, "parse: Starting tag for" + tagName)
+                        if (tagName == "entry") {
+                            inEntry = true
+                        }
+                    }
+
+                    XmlPullParser.TEXT -> textValue = xpp.text
+
+                    XmlPullParser.END_TAG -> {
+                        Log.d(TAG, "parse: Ending tag for " + tagName )
+                        if (inEntry) {
+                            when (tagName) {
+                                "entry" -> {
+                                    applications.add(currentRecord)
+                                    inEntry = false
+                                    currentRecord = FeedEntry() // create a new project
+                                }
+
+                                "name" -> currentRecord.name = textValue
+                                "artist" -> currentRecord.artist = textValue
+                                "releasedate" -> currentRecord.releaseDate = textValue
+                                "summary" -> currentRecord.summary = textValue
+                                "image" -> currentRecord.imageURL = textValue
+                            }
+                        }
+                    }
+                }
+                // Nothing else to do
+                eventType = xpp.next()
+
+            }
+            
+            for (app in applications) {
+                Log.d(TAG, "*******************")
+                Log.d(TAG, app.toString())
             }
 
         } catch (e: Exception) {
